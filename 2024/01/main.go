a3e5b56bbeea5419c21d887e6c2d857c4a469d32
Prakash Divy <prakashdivyy@gmail.com>
2024-12-01 14:38:58 +0100

    Solve part one 01-2024

---
+++ 2024/01/main.go
@@ -0,0 +1,69 @@
+package main
+
+import (
+	"fmt"
+	"os"
+	"strconv"
+	"strings"
+)
+
+func main() {
+	input, err := os.ReadFile("input")
+	if err != nil {
+		panic(err)
+	}
+
+	var leftArray []int
+	var rightArray []int
+
+	lines := strings.Split(string(input), "\n")
+	for _, line := range lines {
+		if line == "" {
+			continue
+		}
+		parts := strings.Fields(line)
+
+		left, err := strconv.Atoi(parts[0])
+		if err != nil {
+			panic(err)
+		}
+		leftArray = append(leftArray, left)
+
+		right, err := strconv.Atoi(parts[1])
+		if err != nil {
+			panic(err)
+		}
+		rightArray = append(rightArray, right)
+	}
+
+	solvePartOne(leftArray, rightArray)
+}
+
+func solvePartOne(l []int, r []int) {
+	leftArray := sortArray(l)
+	rightArray := sortArray(r)
+
+	total := 0
+
+	for i := 0; i < len(leftArray); i++ {
+		diff := leftArray[i] - rightArray[i]
+		if diff < 0 {
+			diff = -diff
+		}
+		total += diff
+	}
+
+	fmt.Printf("[Part One] Total: %d\n", total)
+}
+
+func sortArray(arr []int) []int {
+	n := len(arr)
+	for i := 0; i < n-1; i++ {
+		for j := 0; j < n-i-1; j++ {
+			if arr[j] > arr[j+1] {
+				arr[j], arr[j+1] = arr[j+1], arr[j]
+			}
+		}
+	}
+	return arr
+}
